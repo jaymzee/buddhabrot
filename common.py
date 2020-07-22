77661a32506290208eba72c964219b020147309f
jaymzee <jaymzee@users.noreply.github.com>
2020-07-22 18:53:54 -0400

    cleanup

--- common.py
+++ common.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 
-def print_stats(arr, header):
+def array_stats(arr, header):
+    """print statistics of the numpy array"""
     if header:
         print(header)
         indent = "    "
@@ -12,24 +13,25 @@ def print_stats(arr, header):
           (indent, np.median(arr), np.mean(arr), np.std(arr)))
 
 
-def load_image(filename):
+def load_array(filename):
+    """read text file into numpy array"""
     print("reading %s" % filename)
     arr = np.loadtxt(filename)
-    print_stats(arr, "input file statistics:")
+    array_stats(arr, "input file statistics:")
     return arr
 
 
-def clip_image(arr, threshold):
+def clip_array(arr, threshold):
+    """clip the image array at threshold"""
     # clip values
     if threshold is None:
         return arr
-    else:
-        print("clipping values abouve %d" % threshold)
-        clipped_arr = np.minimum(arr, threshold)
-        diff_arr = arr - clipped_arr
-        diff_count = np.count_nonzero(diff_arr)
-        print("clipped %d pixels (%.3f %%)" %
-              (diff_count, 100.0 * diff_count / diff_arr.size))
-        print_stats(clipped_arr, "statistics after clipping:")
-        return clipped_arr
 
+    print("clipping values abouve %d" % threshold)
+    clipped_arr = np.minimum(arr, threshold)
+    diff_arr = arr - clipped_arr
+    diff_count = np.count_nonzero(diff_arr)
+    print("clipped %d pixels (%.3f %%)" %
+          (diff_count, 100.0 * diff_count / diff_arr.size))
+    array_stats(clipped_arr, "statistics after clipping:")
+    return clipped_arr
