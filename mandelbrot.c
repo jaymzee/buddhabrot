5d73cadec976572b8a7d815293e300e969000a3b
jaymzee <jaymzee@users.noreply.github.com>
2020-07-22 22:23:38 -0400

    mandelbrot writes to numpy compatible text file

--- mandelbrot.c
+++ mandelbrot.c
@@ -3,15 +3,15 @@
 #include <string.h>
 #include <unistd.h>
 
-enum output { ASCII, PGM };
+enum output { ASCII, PGM, NP };
 enum output output_type = ASCII;
-int  maxiter = 256;
-int  resx = 61;
-int  resy = 21;
-double x0 = -2.0;
-double x1 = 1.0;
-double y0 = 1.25;
-double y1 = -1.25;
+int  MAX_ITER = 256;
+int  RES_X = 61;
+int  RES_Y = 21;
+double X0 = -2.0;
+double X1 = 1.0;
+double Y0 = -1.25;
+double Y1 = 1.25;
 
 void arg_error(char *msg) {
     fputs(msg, stderr);
@@ -25,42 +25,51 @@ void proc_args(int argc, char *argv[]) {
 
     for (i = 1; i < argc; i++) {
         if (strncmp("-m", argv[i], 2) == 0) {
-            maxiter = atoi(argv[i] + 2);
+            MAX_ITER = atoi(argv[i] + 2);
         }
-        if (strcmp("-p", argv[i]) == 0) {
-            output_type = PGM;
+        if (strncmp("-f", argv[i], 2) == 0) {
+            switch(argv[i][2]) {
+            case 'p':
+                output_type = PGM;
+                break;
+            case 'n':
+                output_type = NP;
+                break;
+            default:
+                output_type = ASCII;
+            }
         }
         if (strncmp("-w", argv[i], 2) == 0) {
-            resx = atoi(argv[i] + 2);
+            RES_X = atoi(argv[i] + 2);
         }
         if (strncmp("-h", argv[i], 2) == 0) {
-            resy = atoi(argv[i] + 2);
+            RES_Y = atoi(argv[i] + 2);
         }
         if (strncmp("-r", argv[i], 2) == 0) {
             strncpy(buf, argv[i] + 2, 80);
             p = strtok(buf, ",");
             for (j = 0; j < 4; j++) {
                 if (p == NULL) {
-                    arg_error("range should be top left to bottom right"
+                    arg_error("range should be bottom left to top right"
                               ", example: -r-2.0,1.25,1.0,-1.25");
-                } 
+                }
                 r[j] = p;
                 p = strtok(NULL, ",");
             }
-            x0 = atof(r[0]);
-            x1 = atof(r[2]);
-            y0 = atof(r[1]);
-            y1 = atof(r[3]);
+            X0 = atof(r[0]);
+            X1 = atof(r[2]);
+            Y0 = atof(r[1]);
+            Y1 = atof(r[3]);
         }
     }
 
-    if (resx < 5) {
+    if (RES_X < 5) {
         arg_error("minimum x resolution is 10 pixels");
     }
-    if (resy < 5) {
+    if (RES_Y < 5) {
         arg_error("minimum y resolution is 10 pixels");
     }
-    if (maxiter < 1 || maxiter > 1000000) {
+    if (MAX_ITER < 1 || MAX_ITER > 1000000) {
         arg_error("maximum iterations must be between 1 and 1000000");
     }
 }
@@ -68,13 +77,18 @@ void proc_args(int argc, char *argv[]) {
 void write_header(void) {
     if (output_type == ASCII && isatty(STDOUT_FILENO)) {
         /* clear the screen */
-        fputs("\x1b[2J\x1b[0;0H", stdout); 
+        fputs("\x1b[2J\x1b[0;0H", stdout);
     }
     if (output_type == PGM) {
-        printf("P2\n# iterations=%d", maxiter);
-        printf(", coordinate range=(%g, %g)", x0, y0);
-        printf(" to (%g, %g)\n", x1, y1);
-        printf("%d %d\n255\n", resx, resy);
+        printf("P2\n# iterations=%d", MAX_ITER);
+        printf(", coordinate range=(%g, %g)", X0, Y0);
+        printf(" to (%g, %g)\n", X1, Y1);
+        printf("%d %d\n255\n", RES_X, RES_Y);
+    }
+    if (output_type == NP) {
+        printf("# iterations=%d", MAX_ITER);
+        printf(", coordinate range=(%g, %g)", X0, Y0);
+        printf(" to (%g, %g)\n", X1, Y1);
     }
 }
 
@@ -84,25 +98,36 @@ void write_pixel(int n) {
 
     if (output_type == ASCII) {
         count += 1;
-        if (n == maxiter) {
+        if (n == MAX_ITER) {
             putchar('*');
         } else {
             putchar(' ');
         }
-        if (count >= resx) {
+        if (count >= RES_X) {
             putchar('\n');
             count = 0;
         }
-    } else { /* PGM limit line length to 70 chars */
+    }
+    if (output_type == PGM) {
+        /* PGM limit line length to 70 chars */
         count += 4;
-        color = 255 * n / maxiter;
+        color = 255 * n / MAX_ITER;
         /* color = 255 - color; */
-        if (n == maxiter) {
-           color = 0; 
+        if (n == MAX_ITER) {
+           color = 0;
         }
 
         printf("%3d ", color);
-        if (count >= 64) { 
+        if (count >= 64) {
+            putchar('\n');
+            count = 0;
+        }
+    }
+    if (output_type == NP) {
+        count += 1;
+        color = n;
+        printf("%d ", color);
+        if (count >= RES_X) {
             putchar('\n');
             count = 0;
         }
@@ -119,14 +144,14 @@ int main(int argc, char *argv[]) {
     proc_args(argc, argv);
     write_header();
 
-    dx = (x1 - x0) / (resx - 1);
-    dy = (y1 - y0) / (resy - 1);
-    
+    dx = (X1 - X0) / (RES_X - 1);
+    dy = (Y1 - Y0) / (RES_Y - 1);
+
     /* (x, -2.0, 1.0, 0.025) (y, -1.25, 1.25, 0.0625) */
-    for (y = y0, i = 0; i < resy; y += dy, i++) {
-        for(x = x0, j = 0; j < resx; x += dx, j++) {
+    for (y = Y0, i = 0; i < RES_Y; y += dy, i++) {
+        for(x = X0, j = 0; j < RES_X; x += dx, j++) {
             zr = zi = zr2 = zi2 = 0.0;
-            for (n = 0; n < maxiter; n++) {
+            for (n = 0; n < MAX_ITER; n++) {
                 /* z = z^2 + c */
                 zi = 2.0 * zr * zi + y;
                 zr = zr2 - zi2 + x;
@@ -144,4 +169,3 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
-
